ad7ee19e42560250d18bb8a38c416c6af7963403
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2021-10-27 14:08:07 -0700

    checked10/27

--- ClientsInformation.sql
+++ ClientsInformation.sql
@@ -2,44 +2,63 @@ create table clients
 (
     id serial primary key,
     ClientName varchar(100) not null,
-    PhoneNumber varchar(100) not null,
-    ClientEmail varchar(100) not null
+    PhoneNumber varchar(15) not null,
+    ClientEmail varchar(20) not null
 );
 
 create table orderlist 
 (
     clientid int REFERENCES clients(id),
-    productName varchar(100) not null,
+    productName varchar(30) not null,
     quantity int not null
 );
 
 create table leftoverorderlist
 (
     clientid int REFERENCES clients(id),
-    productName varchar(100) not null,
+    productName varchar(30) not null,
     quantity int not null
 );
 
+create table states
+(
+    clientid int REFERENCES clients(id),
+    productName varchar(30) not null,
+    statesofItem varchar(20) not null
+);
 
 create table shoppingorder
 (
+    orderid serial primary key,
     clientid int REFERENCES clients(id),
-    orderid varchar(20) not null,
-    datatime varchar(20) not null,
-    total real not null
+    ClientName varchar(100) not null,
+    PhoneNumber varchar(15) not null,
+    ClientEmail varchar(20) not null,
+    datetime1 varchar(20) not null,
+    total varchar(20) not null
 );
 
 select * from clients;
 select * from orderlist;
 select * from leftoverorderlist;
+select * from states;
 select * from shoppingorder;
 
 
-insert into orderlist(clientid, productName, quantity) values ('4','Veggie','4')
+insert into leftoverorderlist(clientid, productName, quantity) values ('4','Apple','1')
 
 update clients set clientname = 'Garman Lao', phoneNumber = '8001352468', ClientEmail = 'GL@email.com' where id = 7;
 
 Select * from orderlist where clientid =  '4'
 
-drop table orderlist 
-Delete from leftoverorderlist where clientid = '4'
+drop table clients;
+drop table orderlist;
+drop table leftoverorderlist;
+drop table states;
+drop table shoppingorder
+
+Delete from leftoverorderlist;
+Delete from orderlist;
+Delete from states;
+Delete from shoppingorder;
+
